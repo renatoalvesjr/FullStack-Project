e7b49d241a81d784b03b047b2d2c180d4d5dca4e
Renato <grangamin@gmail.com>
2023-09-14 04:16:27 -0300

    Update flaskapi.py

--- flaskapi.py
+++ flaskapi.py
@@ -1,29 +1,10 @@
 from flask import Flask, render_template, request
-import psycopg2
 import random
 import pandas as pd
 import matplotlib.pyplot as plt
-from config import DATABASE
-
-def get_db_connection():
-    conn = psycopg2.connect(
-        host=DATABASE['host'],
-        port=DATABASE['port'],
-        database=DATABASE['database'],
-        user=DATABASE['user'],
-        password=DATABASE['password']
-    )
-    return conn
 
 server = Flask(__name__)
 server.config['STATIC_FOLDER'] = 'static'
-
-s = pd.Series([1, 2, 3])
-fig, ax = plt.subplots()
-s.plot.bar()
-fig.savefig('my_plot.png')
-
-df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
 
 @server.route('/send_data', methods=["GET","POST"])
 def get_data():
